a90a45ff230be3e868d8e21eceb7b5fd9bad4ece
Lisa Lee <unclily@live.unc.edu>
2022-12-01 18:08:48 -0500

    a04

--- server.js
+++ server.js
@@ -19,37 +19,36 @@ app.get('/app/', (req, res) => {
     res.status(200).send('200 OK');
 })
 
-var sides = 6;
-var dice = 2;
-var rolls = 1;
-
 app.get('/app/roll', (req, res) => {
+    let sides = 6;
+    let dice = 2;
+    let rolls = 1;
     res.status(200).send(roll(sides, dice, rolls));
-})
+});
 
 app.post('/app/roll', (req, res) => {
-    sides = parseInt(req.body.sides) || 6;
-    dice = parseInt(req.body.dice) || 2;
-    rolls = parseInt(req.body.rolls) || 1;
+    sides = parseInt(req.body.sides);
+    dice = parseInt(req.body.dice);
+    rolls = parseInt(req.body.rolls);
     res.status(200).send(roll(sides, dice, rolls));
-})
+});
 
 app.get('/app/roll/:sides/', (req, res) => {
     res.status(200).send(roll(parseInt(req.params.sides), 2, 1));
-})
+});
 
 app.get('/app/roll/:sides/:dice/', (req, res) => {
     res.status(200).send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1));
-})
+});
 
 app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
     res.status(200).send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)));
-})
+});
 
 app.get('*', (req, res) => {
     res.status(404).send('404 NOT FOUND');
-})
+});
 
 app.listen(port, () => {
 	console.log(port);
-})
+});
